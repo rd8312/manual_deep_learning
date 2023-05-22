47e64c510bf8590cab12f3b9fb8e5417dc6c31b8
Jhih-Yu Chen <a402250025@gmail.com>
2023-05-23 00:26:03 +0800

    add __name__

--- step01_variable.py
+++ step01_variable.py
@@ -4,10 +4,11 @@ class Variable:
     def __init__(self, data):
         self.data = data
 
-data = np.array(1.0)
-x = Variable(data)
-print(x.data)
+if __name__ == "__main__":
+    data = np.array(1.0)
+    x = Variable(data)
+    print(x.data)
 
-x.data = np.array(2.0)
-print(x.data)
+    x.data = np.array(2.0)
+    print(x.data)
 
